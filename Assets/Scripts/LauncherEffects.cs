6e3a2d23fc6f024fe3107d810d410aeb7854ec7e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-04-03 03:03:33 -0400

    Coconut launcher shakes while charging

--- Assets/Scripts/LauncherEffects.cs
+++ Assets/Scripts/LauncherEffects.cs
@@ -9,21 +9,39 @@ public class LauncherEffects : MonoBehaviour
     public float maxLaunchKnockBack = 2;
     [Header("Components")]
     public PlayerController playerController;
+    public Transform shakeTransform;
 
+    private Vector2 origLocalPos;
     private float shakeEffectIntensity = 0;
     private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
+        origLocalPos = shakeTransform.localPosition;
         rb2d = playerController.GetComponent<Rigidbody2D>();
+        playerController.onChargeChanged += updateShake;
         playerController.onCoconutLaunched += applyKnockBack;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 lpos = origLocalPos;
+        lpos.x += Random.Range(-shakeEffectIntensity, shakeEffectIntensity);
+        shakeTransform.localPosition = lpos;
+    }
 
+    void updateShake(float charge)
+    {
+        shakeEffectIntensity = playerController.ChargePercent * maxShakeEffectIntensity;
+        if (charge <= 0)
+        {
+            shakeEffectIntensity = 0;
+            Vector2 lpos = shakeTransform.localPosition;
+            lpos.x = origLocalPos.x;
+            shakeTransform.localPosition = lpos;
+        }
     }
 
     void applyKnockBack(CoconutController coconut)
