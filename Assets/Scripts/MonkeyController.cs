8ad47227a6c87a3c4bada928927f16f5c93383cc
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-04-02 23:27:48 -0400

    Monkeys can change direction

--- Assets/Scripts/MonkeyController.cs
+++ Assets/Scripts/MonkeyController.cs
@@ -6,14 +6,37 @@ using UnityEngine;
 public class MonkeyController : MonoBehaviour
 {
     public float moveSpeed = 3;
+    public int moveVotesUp = 10;
+    public int maxMoveVotesDown = 5;
+    public int maxMoveVotesSideways = 8;
 
     private Vector2 moveDir = Vector2.up;
+    private List<Vector2> movePossibilities;
 
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
+        //RB2D
         rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 0;
+        //Move possibilities
+        movePossibilities = new List<Vector2>();
+        for (int i = 0; i < moveVotesUp; i++)
+        {
+            movePossibilities.Add(Vector2.up);
+        }
+        int moveVotesDown = Random.Range(0, maxMoveVotesDown + 1);
+        for (int i = 0; i < moveVotesDown; i++)
+        {
+            movePossibilities.Add(Vector2.down);
+        }
+        int moveVotesSideways = Random.Range(0, maxMoveVotesSideways + 1);
+        for (int i = 0; i < moveVotesSideways; i++)
+        {
+            movePossibilities.Add(Vector2.left);
+            movePossibilities.Add(Vector2.right);
+        }
     }
 
     // Update is called once per frame
@@ -22,12 +45,32 @@ public class MonkeyController : MonoBehaviour
         rb2d.velocity = moveDir * moveSpeed;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Branch>())
+        {
+            changeDirection();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<CoconutController>())
         {
-            Destroy(this);
-            GetComponents<Collider2D>().ToList().ForEach(coll => coll.isTrigger = true);
+            FallOff();
         }
+    }
+
+    private void FallOff()
+    {
+        rb2d.gravityScale = 1;
+        Destroy(this);
+        GetComponents<Collider2D>().ToList().ForEach(coll => coll.isTrigger = true);
+    }
+
+    private void changeDirection()
+    {
+        int rand = Random.Range(0, movePossibilities.Count);
+        moveDir = movePossibilities[rand];
     }
 }
