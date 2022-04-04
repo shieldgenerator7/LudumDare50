43a542c2e36a87a54068a6a03a5e71609065c1a7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-04-04 03:55:44 -0400

    Make Utility script

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    /// <summary>
+    /// Returns a list of child game objects that have the given string in its name
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static List<GameObject> getChildrenWithName(this GameObject go, string name)
+    {
+        List<GameObject> children = new List<GameObject>();
+        foreach(Transform transform in go.transform)
+        {
+            if (transform.name.Contains(name))
+            {
+                children.Add(transform.gameObject);
+            }
+        }
+        return children;
+    }
+
+    /// <summary>
+    /// Return the first child with the given string in its name
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static GameObject getChildWithName(this GameObject go, string name)
+    {
+        List<GameObject> children = go.getChildrenWithName(name);
+        if (children.Count > 0)
+        {
+            return children[0];
+        }
+        return null;
+    }
+}
