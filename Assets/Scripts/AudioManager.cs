a89f94eeea71084e8feacbee41f781a977925ab7
Darrias <abysmalparadox@gmail.com>
2022-04-04 18:07:28 -0400

    sfx and ai

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource gunCharge;
+	AudioSource gunFire;
+	AudioSource hitGeneric;
+	AudioSource hitEnemy;
+	AudioSource bananaGet;  
+	
+	
+    void Start()
+    {
+        //audioData = GetComponent<AudioSource>();
+        //audioData.Play(0);
+    }
+	
+	void GunCharge(){
+		//gunCharge = GetComponent<AudioSource>()
+		//gunCharge.Play(0);	
+	}
+	
+	void GunFire(){
+		//gunFire = GetComponent<AudioSource>()
+		//gunFire.Play(0);	
+	}
+	void HitGeneric(){
+		//hitGeneric = GetComponent<AudioSource>()
+		//hitGeneric.Play(0);	
+	}
+	void HitEnemy(){
+		//hitEnemy = GetComponent<AudioSource>()
+		//hitEnemy.Play(0);	
+	}
+	void BananaGet(){
+		//hitGetComponent<AudioSource>()
+		//bananaGet.Play(0);	
+	}
+	
+
+}
