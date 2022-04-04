2513d90489ff4a44fb07a5190a3e999d9f4a49cb
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-04-04 01:01:21 -0400

    Implement some menu buttons

---
+++ Assets/Scripts/Menu/CreditsButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MenuButton
+{
+    protected override void takeAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
