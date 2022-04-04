de23f5a02aa8323ed61643c6e831b44889185d27
Darrias <abysmalparadox@gmail.com>
2022-04-04 01:11:30 -0400

    gridstuff

--- Assets/Scripts/Pathfinding/GridBehavior.cs
+++ Assets/Scripts/Pathfinding/GridBehavior.cs
@@ -6,7 +6,7 @@ public class GridBehavior : MonoBehaviour
 {
 	public int rows = 10; 
 	public int columns = 10;
-	public int scale = 1;
+	public float scale = 1;
 	public GameObject gridPrefab;
 	public Vector3 leftBottomLocation = new Vector3(0,0,0);
 	
@@ -31,7 +31,7 @@ public class GridBehavior : MonoBehaviour
 		{
 			for(int j =0; j<rows; j++)
 			{
-				GameObject obj = Instantiate(gridPrefab, new Vector3 (leftBottomLocation.x+scale*i,leftBottomLocation.y, leftBottomLocation.z+scale*j), Quaternion.identity);
+				GameObject obj = Instantiate(gridPrefab, new Vector3 (leftBottomLocation.x+scale*i,leftBottomLocation.y+scale*j, leftBottomLocation.z), Quaternion.identity);
 				obj.transform.SetParent(gameObject.transform);
 				obj.GetComponent<GridStat>().x = i;
 				obj.GetComponent<GridStat>().y = j;
