d5701b92de97506326bd419fe90ac485dabc2bfc
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-04-04 04:01:09 -0400

    Add DeleteChildrenWithName()

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class Utility
@@ -13,7 +14,7 @@ public static class Utility
     public static List<GameObject> getChildrenWithName(this GameObject go, string name)
     {
         List<GameObject> children = new List<GameObject>();
-        foreach(Transform transform in go.transform)
+        foreach (Transform transform in go.transform)
         {
             if (transform.name.Contains(name))
             {
@@ -37,5 +38,20 @@ public static class Utility
             return children[0];
         }
         return null;
+    }
+
+    public static void DeleteChildrenWithName(this GameObject go, string[] names)
+    {
+        go.DeleteChildrenWithName(names.ToList());
+    }
+
+    public static void DeleteChildrenWithName(this GameObject go, List<string> names)
+    {
+        List<GameObject> children = new List<GameObject>();
+        foreach (string name in names)
+        {
+            children.AddRange(go.getChildrenWithName(name));
+        }
+        children.ForEach(child => Object.Destroy(child));
     }
 }
