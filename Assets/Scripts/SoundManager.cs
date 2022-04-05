da8ed326b3ce7d71c982e66c034d8054a1e12250
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-04-04 21:43:36 -0400

    Add SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("AudioSources")]
+    public AudioSource gunCharge;
+    public AudioSource gunFire;
+    public AudioSource hitEnemy;
+    public AudioSource hitOther;
+    public AudioSource bananaGet;
+
+    [Header("Components")]
+    public PlayerController playerController;
+    public BananaManager bananaManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController.onChargeChanged += onChargeChanged;
+        playerController.onCoconutLaunched += onGunLaunch;
+        bananaManager.onBananaTotalChanged += bananasChanged;
+        gunCharge.Stop();
+    }
+
+    void onChargeChanged(float charge)
+    {
+        if (charge > 0 && charge < 0.5f)
+        {
+            gunCharge.Play();
+        }
+        else if (charge == 0)
+        {
+            gunCharge.Stop();
+        }
+    }
+
+    void onGunLaunch(CoconutController coconut)
+    {
+        //Delegate
+        coconut.onHitCountUpdated += onCoconutHit;
+        coconut.onHitOther += onCoconutHitOther;
+        //Sound
+        gunFire.Play();
+    }
+
+    void onCoconutHit(int hitCount)
+    {
+        hitEnemy.Play();
+    }
+    void onCoconutHitOther()
+    {
+        hitOther.Play();
+    }
+
+    void bananasChanged(int bananas)
+    {
+        bananaGet.Play();
+    }
+}
